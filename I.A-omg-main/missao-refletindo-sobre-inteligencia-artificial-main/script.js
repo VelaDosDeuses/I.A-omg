cf0a45285def2b175ea45c1b1591275f48c10442
VelaDosDeuses <114483311+VelaDosDeuses@users.noreply.github.com>
2024-06-14 08:27:19 -0300

    Add files via upload

---
+++ I.A-omg-main/missao-refletindo-sobre-inteligencia-artificial-main/script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciaado: "pergunta 1", 
+    alternativas: [
+        "alternativas 1",
+        "alternativas 2"]
+    }
+    "pergunta 2"
+]
